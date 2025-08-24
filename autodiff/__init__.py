afd6ebc50c55f4847a370a609e6202c8d89c1531
Om <ompanchal01102006@gmail.com>
2025-08-24 23:39:42 +0100

    import changes

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -1,5 +1,6 @@
 from autodiff import nodes
 from autodiff.Tensor import Tensor
 from autodiff import ufunc_gradients
+from autodiff.Graph import Graph
 
-__all__ = [nodes, Tensor, ufunc_gradients]
+__all__ = [nodes, Tensor, ufunc_gradients, Graph]
--- autodiff/nodes/__init__.py
+++ autodiff/nodes/__init__.py
@@ -2,7 +2,6 @@ from autodiff.nodes.Const import Const
 from autodiff.nodes.Node import Node
 from autodiff.nodes.Operator import Operator
 from autodiff.nodes.Var import Var
-from autodiff.nodes.Graph import Graph
 
 
-__all__ = [Node, Graph, Operator, Var, Const]
+__all__ = [Node, Operator, Var, Const]
