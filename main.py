74749f87d8a4d22b182f2e7faaffd8a283527a91
Om <ompanchal01102006@gmail.com>
2025-08-24 23:39:33 +0100

    trying to calculate second derivatives

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import numpy as np
 from autodiff.Tensor import Tensor
-from autodiff.nodes import Graph
+from autodiff import Graph
 
 
 a = Tensor(1, dtype="float64")
 # b = Tensor(4)
 # c = Tensor(2)
 
-d = np.sin(np.tan(np.cos(np.cosh((a ** 7) + (a ** 3) - a) + a ** 4) * a) - a)
-
-e = d * 2
+# d = np.sin(np.tan(np.cos(np.cosh((a ** 7) + (a ** 3) - a) + a ** 4) * a) - a)
+d = (a ** 3)
 
 g = Graph()
 
-g.add_variables(a, d)
+g.add_variables(a)
 
 # gradient = g.gradient(d, [a])
-gradient = g.gradient(e, [a])
+gradient = g.gradient(d, [a])[0]
 print(gradient)
+print(g.grad_func_ops)
+print(g.gradient(gradient, [a]))
+print(g.grad_func_ops)
+# print(g.grad_func_ops)
